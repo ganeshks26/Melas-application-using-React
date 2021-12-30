6f4e2c28595d7d7a45bb5477fbf6c7e66dd2c50f
Ganesh K S <ganeshks2611@gmail.com>
2021-12-30 13:31:28 +0530

    meals app using react

---
+++ src/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Footer(){
+    return <div className="footer">
+        <p style={{color:'white'}}>Designed and Developed By</p>
+        <p style={{color:'white'}}>Ganesh K S</p>
+    </div>
+}
+export default Footer
---
+++ src/Navbar.js
@@ -0,0 +1,9 @@
+import React from 'react'
+function Navbar(){
+
+   return <div className="navbar">
+       <h1>React Meals</h1> 
+   </div>
+
+}
+export default Navbar
